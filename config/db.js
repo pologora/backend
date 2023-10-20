08eec8483ce8e1faeedd925a434aae8e83cc78ab
Oleksandr <lysakova@yahoo.com>
2023-10-20 18:10:31 +0200

    test 8

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// const uri = process.env.NODE_ENV === 'development'
-//   ? process.env.MONGODB_URI_DEV
-//   : process.env.MONGODB_URI_PROD;
-const uri = 'mongodb+srv://admin:admin@natures.fmvhhwp.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.NODE_ENV === 'development'
+  ? process.env.MONGODB_URI_DEV
+  : process.env.MONGODB_URI_PROD;
+// const uri = 'mongodb+srv://admin:admin@natures.fmvhhwp.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
