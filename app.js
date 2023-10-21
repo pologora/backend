8be189d16ff8c03f6469d76e86bbe19e479f33da
Oleksandr <lysakova@yahoo.com>
2023-10-21 08:32:14 +0200

    add yarn

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/', (req, res, next) => {
     message: 'dupa',
   });
 });
+
 // app.use('/api/v1/users', vacationRouter);  vacations
 // app.use('/api/v1/users', worktimeRouter);  worktime
 
